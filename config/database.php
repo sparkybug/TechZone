f6e8160e40ea2a49cd1f822157ce1bd5a885d096
sinach <ukaegbuosinachi15@gmail.com>
2023-04-24 23:12:45 +0100

    updated the collation from utf8mb4_unicode_ci to utf8_unicode_ci in config/database.php

--- config/database.php
+++ config/database.php
@@ -59,7 +59,7 @@ return [
             // 'charset' => 'utf8mb4',
             // 'collation' => 'utf8mb4_unicode_ci',
             'charset' => 'utf8',
-            'collation' => 'utf8mb4_unicode_ci',
+            'collation' => 'utf8_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,

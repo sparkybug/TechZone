916f57d43ba466c4e8a31a924fb7cbe527b8bf57
sinach <ukaegbuosinachi15@gmail.com>
2023-04-26 02:41:33 +0100

    created model, controller and migration file for jobs

---
+++ app/Http/Controllers/JobsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Jobs;
+use Illuminate\Http\Request;
+
+class JobsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Jobs $jobs)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Jobs $jobs)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Jobs $jobs)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Jobs $jobs)
+    {
+        //
+    }
+}
---
+++ app/Models/Jobs.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Jobs extends Model
+{
+    use HasFactory;
+
+    protected $guard = 'jobs';
+
+    protected $table = 'jobs';
+
+    protected $fillable = [
+        'job_tag',
+        'skill_set',
+        'work_period',
+        'budget_des',
+        'budget',
+        'job_des',
+
+        // des means description
+    ]
+}

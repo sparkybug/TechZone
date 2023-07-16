e11b818d3bab2a98b4e3121ace65bddf7398a305
sinach <ukaegbuosinachi15@gmail.com>
2023-07-16 20:37:42 +0100

    deleted other company details

---
+++ database/migrations/2023_07_16_193633_update_and_addstatus_to_employers_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('employers', function (Blueprint $table) {
+            $table->dropColumn('company_rating');
+            $table->dropColumn('company_location');
+            $table->dropColumn('total_spent');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('employers', function (Blueprint $table) {
+            //
+        });
+    }
+};

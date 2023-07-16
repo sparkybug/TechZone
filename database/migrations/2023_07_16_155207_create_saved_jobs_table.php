4578c7e86e371f7382a6ebc8cdaeaf4c80b9af64
sinach <ukaegbuosinachi15@gmail.com>
2023-07-16 16:58:14 +0100

    created the savedJobs model and updated its fields

---
+++ database/migrations/2023_07_16_155207_create_saved_jobs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('saved_jobs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('job_id')->constrained();
+            $table->longText('job_tag');
+            $table->longText('skill_set');
+            $table->longText('work_period');
+            $table->longText('budget_des');
+            $table->longText('budget');
+            $table->longText('job_des');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('saved_jobs');
+    }
+};

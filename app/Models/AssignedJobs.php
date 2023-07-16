dfb8ac97696ed30fbe0a52c4a7e35341bf82797d
sinach <ukaegbuosinachi15@gmail.com>
2023-07-16 17:55:17 +0100

    modified the AssignedJobs model in order to allow for users to decline jobs

--- app/Models/AssignedJobs.php
+++ app/Models/AssignedJobs.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class AssignedJobs extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'job_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }

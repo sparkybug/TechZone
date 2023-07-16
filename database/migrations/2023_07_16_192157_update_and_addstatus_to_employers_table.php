5bde059525c3dbc95fcbd9240b308e42747b230e
sinach <ukaegbuosinachi15@gmail.com>
2023-07-16 20:27:37 +0100

    rectifying issues

--- database/migrations/2023_07_16_192157_update_and_addstatus_to_employers_table.php
+++ database/migrations/2023_07_16_192157_update_and_addstatus_to_employers_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('employers', function (Blueprint $table) {
-            $table->string('company_name', '');
+            $table->string('company_name', '')->change();
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('employers', function (Blueprint $table) {
-            $table->string('company_name', '');
+            // $table->string('company_name', '');
         });
     }
 };

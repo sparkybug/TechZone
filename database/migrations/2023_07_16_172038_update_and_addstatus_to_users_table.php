9de6ec04759ca4c781a43e43edf7fcfe4b009a7a
sinach <ukaegbuosinachi15@gmail.com>
2023-07-16 18:24:41 +0100

    updated users table to include field for verification f email

---
+++ database/migrations/2023_07_16_172038_update_and_addstatus_to_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->boolean('verified')->default(false);
+            $table->string('verification_token')->nullable();
+            $table->timestamp('verified_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->boolean('verified')->default(false);
+            $table->string('verification_token')->nullable();
+            $table->timestamp('verified_at')->nullable();
+        });
+    }
+};

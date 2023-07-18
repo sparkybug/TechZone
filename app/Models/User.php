1bd2b9ccbb8438a2336ddc13b3c5fb9112d94bdf
sinach <ukaegbuosinachi15@gmail.com>
2023-07-18 22:49:59 +0100

    set primaryKey in User model to user_id

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,6 +17,7 @@ class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, Notifiable, HasFactory;
 
+    protected $primaryKey = 'user_id';
     /**
      * The attributes that are mass assignable.
      *

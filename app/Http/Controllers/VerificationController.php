d6bd025e88389990e0574b9de44aed9108187cce
sinach <ukaegbuosinachi15@gmail.com>
2023-07-19 01:19:51 +0100

    replaced id with user_id in EmailVerificationRequest.php

--- app/Http/Controllers/VerificationController.php
+++ app/Http/Controllers/VerificationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Auth\Events\Verified;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Http\Request;

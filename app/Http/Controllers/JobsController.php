b6d829949d68cc835fe00052bc6cde92414dd8bd
sinach <ukaegbuosinachi15@gmail.com>
2023-09-10 20:50:52 +0100

    trying to save jobs to db

--- app/Http/Controllers/JobsController.php
+++ app/Http/Controllers/JobsController.php
@@ -32,7 +32,7 @@ class JobsController extends Controller
         $jobs = Jobs::findOrFail($id);
 
         //  Save the job to the database
-        $saved = $request->user()->toggle($jobs);
+        $saved = $request->user()->savedJobs()->toggle($jobs);
 
         if($saved->contains($jobs)) {
             return response()->json(['message' => 'Job saved successfully']);

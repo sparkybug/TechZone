6a9c99801083b79a6d35dca81ac7cd28cbaeab9c
sinach <ukaegbuosinachi15@gmail.com>
2023-09-10 20:46:29 +0100

    modified saved jobs model

--- app/Models/SavedJobs.php
+++ app/Models/SavedJobs.php
@@ -8,4 +8,31 @@ use Illuminate\Database\Eloquent\Model;
 class SavedJobs extends Model
 {
     use HasFactory;
+
+    protected $guard = 'jobs';
+
+    protected $table = 'jobs';
+
+    protected $primaryKey = 'job_id';
+
+    protected $fillable = [
+        'job_tag',
+        'skill_set',
+        'work_period',
+        'budget_des',
+        'budget',
+        'job_des',
+
+        // des means description
+    ];
+
+    protected $casts = [
+        'jobs_created_at' => 'datetime',
+    ];
+
+    // Creating a relationship to User model
+    public function savedJobs()
+    {
+        return $this->belongsToMany(User::class, 'saved_jobs');
+    }
 }

f2c78aba39fb9489973826511da4169fe5d9094d
sinach <ukaegbuosinachi15@gmail.com>
2023-09-12 20:13:27 +0100

    updated savedController

--- app/Http/Controllers/SavedJobController.php
+++ app/Http/Controllers/SavedJobController.php
@@ -3,8 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\SavedJobs;
 
 class SavedJobController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'user_id' => 'required|exists:users,id',
+            'job_id' => 'required|exists:jobs,id',
+        ]);
+
+        $savedJob = SavedJobs::create($validatedData);
+
+        return response()->json(['message' => 'Job saved successfully', 'data' => $savedJob], 201);
+    }
+
+    public function index(Request $request)
+    {
+        $user_id = $request->input('user_id');
+        $savedJobs = SavedJobs::where('user_id', $user_id)->get();
+
+        return response()->json(['data' => $savedJobs]);
+    }
+
+    public function destroy($id)
+    {
+        $savedJob = SavedJobs::findOrFail($id);
+        $savedJob->delete();
+
+        return response()->json(['message' => 'Job removed from saved jobs']);
+    }
 }

fef2538b15d061cd2bb80fa2741650dc5c05dfb2
sinach <ukaegbuosinachi15@gmail.com>
2023-09-13 17:14:09 +0100

    modified the foreign key property of job_id

--- database/migrations/2023_09_13_154310_update_and_addstatus_to_saved_jobs_table.php
+++ database/migrations/2023_09_13_154310_update_and_addstatus_to_saved_jobs_table.php
@@ -12,7 +12,6 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('saved_jobs', function (Blueprint $table) {
-            $table->id();
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('job_id');
 

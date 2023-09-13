31d0079451068d6981e7a7e57523162faaad615f
sinach <ukaegbuosinachi15@gmail.com>
2023-09-13 21:10:00 +0100

    wrote the route for getInfo by authenticated user

--- routes/api.php
+++ routes/api.php
@@ -40,9 +40,10 @@ Route::post('logout', [PassportAuthController::class, 'logout']);
 Route::post('employer/register', [RegisterController::class, 'register']);
 Route::post('employer/login', [LoginController::class, 'login']);
 
-Route::get('get-user/{id}', [PassportAuthController::class, 'userInfo']);
+Route::get('get-user/{id}', [PassportAuthController::class, 'user_Info']);
 
-  
+Route::middleware('auth:api')->get('/get-user-info', 'PassportAuthController@userInfo');
+
 Route::middleware('auth:api')->group(function () {
     Route::get('get-employer', [PassportAuthController::class, 'EmployerInfo']);
 });

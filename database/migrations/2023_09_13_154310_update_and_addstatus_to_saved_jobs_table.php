693e31cc371216a1ad9f5d0d983a48a47b4e49af
sinach <ukaegbuosinachi15@gmail.com>
2023-09-13 16:48:14 +0100

    created migration file to drop timestamps columns in saved_jobs table

--- database/migrations/2023_09_13_154310_update_and_addstatus_to_saved_jobs_table.php
+++ database/migrations/2023_09_13_154310_update_and_addstatus_to_saved_jobs_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('saved_jobs', function (Blueprint $table) {
-            $table->dropColumn('timestamps');
+            $table->dropTimestamps();
         });
     }
 
